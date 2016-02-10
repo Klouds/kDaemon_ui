9d65814450ee5f03c07c80427789d2b7167b6666
Paul Mauviel <ozzadar@ozzadar.com>
2016-02-10 15:02:40 +0700

    The stores be working, yo!

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 import NavBarComponent from './NavBarComponent/NavBarComponent.jsx'
 import ListComponent from './ListComponent/ListComponent.jsx'
 import ViewComponent from './ViewComponent/ViewComponent.jsx'
+import AltContainer from 'alt-container'
 
 import ApplicationsStore from '../stores/ApplicationsStore.jsx'
 import ApplicationsActions from '../actions/ApplicationsActions.jsx'
+import NodesStore from '../stores/NodesStore.jsx'
+import NodesActions from '../actions/NodesActions.jsx'
+import ContainersStore from '../stores/ContainersStore.jsx'
+import ContainersActions from '../actions/ContainersActions.jsx'
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -29,21 +34,70 @@ export default class App extends React.Component {
 					dependencies: [],
 					is_enabled: true
 				}
-			]
+			],
+			mock_nodes: [
+				{
+					name:'ghost_blog',
+					docker_image: 'ghost',
+					exposed_ports: ["2368"],
+					dependencies: [],
+					is_enabled: true
+				},
+				{
+					name:'codiad',
+					docker_image: 'quantumobject/docker-codiad',
+					exposed_ports: ["80"],
+					dependencies: [],
+					is_enabled: true
+				}
+			],
+			mock_containers: [
+				{
+					name:'ghost_blog',
+					docker_image: 'ghost',
+					exposed_ports: ["2368"],
+					dependencies: [],
+					is_enabled: true
+				},
+				{
+					name:'codiad',
+					docker_image: 'quantumobject/docker-codiad',
+					exposed_ports: ["80"],
+					dependencies: [],
+					is_enabled: true
+				}
+			],
+			currentStore: ApplicationsStore
 		}
 	}
 
 	componentWillMount() {
 		this.loadApplicationsIntoStore();
+		this.loadNodesIntoStore();
+		this.loadContainersIntoStore();
 	}
 
 	loadApplicationsIntoStore() {
 		this.state.mock_applications.map(app => {
 			ApplicationsActions.create(app)
 		})
+	}	
+
+	loadNodesIntoStore() {
+		this.state.mock_nodes.map(node => {
+			NodesActions.create(node)
+		})
+	}	
+
+	loadContainersIntoStore() {
+		this.state.mock_containers.map(container => {
+			ContainersActions.create(container)
+		})
 	}
 
 	render() {
+
+		let value = this.getStoreValue(this.state.nav);
 		return (
 			<div className='mainDiv'>
 				<NavBarComponent 
@@ -51,22 +105,54 @@ export default class App extends React.Component {
 					onAppsClick={this.changeNav.bind(this, 'applications')}
 					onContainersClick={this.changeNav.bind(this, 'containers')} 
 				/>
-				{(this.state.mode === 'show') ? 
-				<ListComponent 
-					nav={this.state.nav}	
-					mode={this.state.mode}/> : 
-					'' 
-				}
 
+				{(this.state.mode === 'show') ? 
+						<AltContainer 
+							stores={[this.state.currentStore]}
+							inject={{
+								value: () => [value]
+						}}
+						>
+						<ListComponent 
+							nav={this.state.nav}	
+							mode={this.state.mode}/> 
+						</AltContainer> : ''
+
+				}	
 				<ViewComponent />
 			</div>
 		)
 	}
 
+	getStoreValue(nav) {
+		if (nav === 'containers') {
+			return ContainersStore.getState().containers
+		} else if (nav === 'nodes') {
+			return NodesStore.getState().nodes
+		} else if (nav === 'applications') {
+			return ApplicationsStore.getState().applications
+		}
+	}
+	selectAppropriateStore(nav) {
+		let currentStore;
+
+		if (nav === 'containers') {
+			currentStore = ContainersStore
+		} else if (nav === 'nodes') {
+			currentStore = NodesStore
+		} else if (nav === 'applications') {
+			currentStore = ApplicationsStore
+		}
+
+		this.setState(currentStore)
+	}
 	//Change navigation state
 	changeNav(nav) {
 		//change the state
 		this.setState({nav})
+
+		//change current store
+		this.selectAppropriateStore(nav)
 	}
 
 }
--- app/components/ListComponent/ListComponent.jsx
+++ app/components/ListComponent/ListComponent.jsx
@@ -4,22 +4,39 @@ export default class ListComponent extends React.Component {
 	constructor(props) {
 		super(props)
 
-		const {nav, mode} = this.props
+		const {nav, mode, value} = this.props
 
 	}
 	render() {
 		let content = this.renderListComponent()
-
 		return (
 			<div className='listComponent'>{content}</div>
 		)
 	}
 
 	renderListComponent() {
+		let object = this.props.value
+		
+		let items = [];
+
+		let key
+		let list
+		for (key in object) {
+			if (object.hasOwnProperty(key)) {
+				items = items.concat(object[key])
+			}
+		}
+
 		return (
 			<div>
 				<h1 className='header'> List of {this.props.nav} </h1>
-				<span>{this.props.mode}'ing'</span>
+				<ul>
+				{items.map(item => {
+						return <li key={item.id}>{item.name}</li>
+						console.log(item.name)
+					})
+				}
+				</ul>
 			</div>
 		)
 	}
