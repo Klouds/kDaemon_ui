06f8ffc4ecf82983688ad90b9dcccbcbc4b357ed
Paul Mauviel <ozzadar@ozzadar.com>
2016-02-27 19:57:57 +0700

    Finalizing this version

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -33,14 +33,14 @@ export default class App extends React.Component {
 	}
 
 	componentDidMount() {
-		let client = request.createClient('http://192.168.100.2:4000/0.0/')
+		let client = request.createClient('http://localhost:4000/0.0/')
 		console.log('created new client ', client)
 		//set up the JSON client
 		this.setState({json_client:client})
 
 
 		//Initialize websocket connection
-		let ws = new WebSocket('ws://192.168.100.2:4000/ws')
+		let ws = new WebSocket('ws://localhost:4000/ws')
     	let socket = this.socket = new Socket(ws); 
     	socket.on('connect', this.onConnect.bind(this));
 
